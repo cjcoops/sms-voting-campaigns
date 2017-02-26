66834fa6552a93be70f7c82abd5928eb51c2f74d
Chris Cooper <cjcooper252@gmail.com>
2017-02-26 15:28:09 +0000

    refactors vote spec tests

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 describe Vote, type: :model do
-  context 'choice is not supplied' do
-    it 'vote is not valid' do
-      vote = Vote.new(epoch_time: 1168122971, validity: "during")
-      expect(vote).to have(1).error_on(:choice)
-      expect(vote).not_to be_valid
-    end
+  it 'vote is not valid if choice is not supplied' do
+    vote = Vote.new(epoch_time: 1168122971, validity: "during")
+    expect(vote).to have(1).error_on(:choice)
+    expect(vote).not_to be_valid
   end
 end

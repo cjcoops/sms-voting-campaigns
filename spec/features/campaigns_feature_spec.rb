3c438babe4bf4532c2f69af51e7f4abf187690ff
Chris Cooper <cjcooper252@gmail.com>
2017-02-26 10:46:28 +0000

    adds campaigns index view which shows all campaigns

--- spec/features/campaigns_feature_spec.rb
+++ spec/features/campaigns_feature_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 feature 'campaigns' do
   before do
     campaign = Campaign.create(name: "ssss_uk_02A")
+    campaign = Campaign.create(name: "ssss_uk_01B")
   end
 
   scenario 'show all campaigns' do
     visit('/campaigns')
     expect(page).to have_css('h1', text: "Campaigns")
-    expect(page).to have_content("ssss_uk_02A")
+    expect(page).to have_css('h3', text: "ssss_uk_02A")
+    expect(page).to have_css('h3', text: "ssss_uk_01B")
   end
 
 end

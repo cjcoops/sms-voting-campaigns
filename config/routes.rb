2635e4f778b748783626f03d1b3dba20975f0006
Chris Cooper <cjcooper252@gmail.com>
2017-02-26 11:05:55 +0000

    refactors campaign show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'campaigns' => 'campaigns#index'
-  get 'campaigns/:id', to: 'campaigns#show', as: 'campaign'
+  get 'campaigns/:id' => 'campaigns#show', as: 'campaign'
 
   root 'campaigns#index'
 

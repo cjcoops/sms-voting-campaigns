2267fa2260ec29e2064e305dd8e882f183066247
Chris Cooper <cjcooper252@gmail.com>
2017-02-26 15:16:14 +0000

    adds campaign scores method

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,14 @@ class Campaign < ApplicationRecord
 
    has_many :votes
 
+   def scores
+     results = Hash.new 0
+
+     votes.each do |vote|
+       results[vote.choice] += 1
+     end
+
+     results
+   end
+
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -4,4 +4,18 @@ describe Campaign do
 
   it { is_expected.to have_many :votes }
 
+  let!(:campaign) {Campaign.create(name: "ssss_uk_02A")}
+  let!(:vote_1) {campaign.votes.create(epoch_time: 1168122971, validity: "during", choice: "Mark")}
+  let!(:vote_2) {campaign.votes.create(epoch_time: 1168122972, validity: "during", choice: "Gemma")}
+  let!(:vote_3) {campaign.votes.create(epoch_time: 1168122971, validity: "during", choice: "Mark")}
+
+  describe '#scores' do
+    it "returns a hashmap of choices and scores" do
+      results = { "Mark" => 2,
+                  "Gemma" => 1 }
+
+      expect(campaign.scores).to eq(results)
+    end
+  end
+
 end

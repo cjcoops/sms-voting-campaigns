7235c1757b948bfdc82840fb1c87d4ea2fd7720d
Chris Cooper <cjcooper252@gmail.com>
2017-02-26 15:38:15 +0000

    refactors campaign scores method

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,16 +4,20 @@ class Campaign < ApplicationRecord
 
    def scores
      results = Hash.new 0
-
      votes.each do |vote|
-       if vote.validity == "during"
-         results[vote.choice] += 1
-       else
-         results["Sent but not counted"] += 1
-       end
+       add_vote_to_results_hash(results, vote)
      end
-
      results
+   end
+
+   private
+
+   def add_vote_to_results_hash(results, vote)
+     if vote.validity == "during"
+       results[vote.choice] += 1
+     else
+       results["Sent but not counted"] += 1
+     end
    end
 
 end
